f14e937fbce30f1c0caf5c9065d057dd3f5a4394
Your Name <xxx@y>
2020-10-05 22:26:29 -0300

    AC03

--- primos.py
+++ primos.py
@@ -2,30 +2,26 @@ import os
 from flask import Flask, jsonify, request
 from math import sqrt
 
-app = Flask(__name__)
+app = Flask(_name_)
 
 @app.route('/')
 def nao_entre_em_panico():
-    a = 100
-    b = 1
-    c = 1
-    n = 3
-    pr = '2,'
+    primos = "2"
+    num =  3
+    cont = 0
+    primo = True
+    while(cont < 99):
+        for i in range(2, num-1, 1 ):
+            if num % i == 0:
+                primo = False
+        if primo:
+            primos += "," + str(num)
+            cont += 1
+        num += 1
+        primo = True
 
-    while c < a:
-        primo = 1
-        for i in range(2, n):
-            if n % i == 0:
-                primo = 0
-                break
-        if(primo):
-            pr = pr + str(n) + ','
-            c += 1
-            if(c % 10 == 0):
-                pr = pr + '<br>'
-        n += 1
-    return pr
+    return primos
 
-if __name__ == '__main__':
-    port = int(os.environ.get('PORT', 5000))
+if _name_ == "_main_":
+    port = int(os.environ.get("PORT", 5000))
     app.run(host='0.0.0.0', port=port)
